ebce3a14e05c3c32ce4262539b570ea01c2b1485
ThiagoCamponez <thiagotradercamponez@gmail.com>
2023-06-23 06:41:29 -0300

    Criei o desafio005

---
+++ desafios/desafio005.py
@@ -0,0 +1,4 @@
+num = int(input('Digite um número inteiro: '))
+num-ant = num - 1
+num-sus = num + 1
+print('O sucessor é: {} e o antecessor é: {}'.format(num-sus, num-ant))
---
+++ projetos/aula07a.py
@@ -0,0 +1,4 @@
+n1 = int(input('Um valor: '))
+n2 = int(input('Outro valor: '))
+total = n1+n2
+print('A soma destes dois números é: {}'.format(total))
