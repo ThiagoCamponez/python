d10121fd996be9007333655ff653a87ea67c5538
ThiagoCamponez <thiagotradercamponez@gmail.com>
2023-06-24 03:09:56 -0300

    Criei o desafio011

---
+++ desafios/desafio011.py
@@ -0,0 +1,5 @@
+a = float(input('Qual a altura da parede? '))
+l = float(input('Qual a largura da parede? '))
+area = a * l
+print('Sua parede tem uma área de {} metros quadrados.'.format(area))
+print('Você irá gastar {} litros de tinta pra pintar'.format(area / 2))
