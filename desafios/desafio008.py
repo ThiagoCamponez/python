f899cc4e70c661a4a5852947457f7be4f2334924
ThiagoCamponez <thiagotradercamponez@gmail.com>
2023-06-25 01:46:07 -0300

    Alterado
    Para mostrar números flutuantes sem casas decimais

--- desafios/desafio008.py
+++ desafios/desafio008.py
@@ -1,4 +1,4 @@
 mt = float(input('Quantos metros? '))
 cm = mt * 100
 mm = mt * 1000
-print('{} metros tem: {} centímetros, ou {} milímetros.'.format(mt, cm, mm))
+print('{} metros tem: {:.0f} centímetros, ou {:.0f} milímetros.'.format(mt, cm, mm))
