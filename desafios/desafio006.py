7920a33136e63f86a6b1193379c0cdc4d328a692
ThiagoCamponez <thiagotradercamponez@gmail.com>
2023-06-28 02:26:36 -0300

    Alterado nome dos arquivos

--- desafios/desafio006.py
+++
@@ -1,4 +0,0 @@
-num = int(input('Digite um número: '))
-print('O dobro de {} é: {}'.format(num, num * 2))
-print('O triplo de {} é: {}'.format(num, num * 3))
-print('A raiz quadrada de {} é: {:.2f}'.format(num, num ** (1/2)))
