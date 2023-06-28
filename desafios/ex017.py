594adb2cc3e0c4873cca6efacbbab61aa0112a56
ThiagoCamponez <thiagotradercamponez@gmail.com>
2023-06-28 02:41:30 -0300

    Colocado descrição do problema

--- desafios/ex017.py
+++ desafios/ex017.py
@@ -1,3 +1,7 @@
+'''Exercício Python 17: Faça um programa que leia
+o comprimento do cateto oposto e do cateto adjacente
+de um triângulo retângulo. Calcule e mostre o comprimento da hipotenusa.'''
+
 '''co = float(input('Comprimento do cateto oposto: '))
 ca = float(input('Comprimento do cateto adjacente: '))
 hi = (co ** 2 + ca ** 2) ** (1/2)
--- desafios/ex019.py
+++ desafios/ex019.py
@@ -1,3 +1,8 @@
+'''Exercício Python 19: Um professor quer sortear um dos seus
+quatro alunos para apagar o quadro.
+Faça um programa que ajude ele, lendo o nome dos alunos e escrevendo
+na tela o nome do escolhido.'''
+
 from random import choice
 n1 = str(input('Primeiro aluno: '))
 n2 = str(input('Segundo aluno: '))
