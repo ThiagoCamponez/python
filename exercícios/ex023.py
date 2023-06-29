303c87d3e385038c7849b7fcb9c3ed838c710a0d
ThiagoCamponez <thiagotradercamponez@gmail.com>
2023-06-29 06:42:43 -0300

    Criei o ex023

---
+++ exercícios/ex023.py
@@ -0,0 +1,11 @@
+'''Exercício Python 23: 
+Faça um programa que leia um número de 0 a 9999 e 
+mostre na tela cada um dos dígitos separados.'''
+
+num = int(input('Informe um número: '))
+n = str(num)
+print('Analizando o número {}'.format(num))
+print('Unidade: {}'.format())
+print('Dezena: {}'.format())
+print('Centena: {}'.format())
+print('Milhar: {}'.format())
