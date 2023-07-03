295d203086a97b10b30956d55c0b40e35fbdeba8
ThiagoCamponez <thiagotradercamponez@gmail.com>
2023-07-03 04:48:01 -0300

    Criei o ex033

---
+++ exercícios/ex033.py
@@ -0,0 +1,6 @@
+import math
+n1 = int(input('Digite um número: '))
+n2 = int(input('Digite outro número: '))
+n3 = int(input('Digite outro número: '))
+print('O maior número digitado foi {}'.format(max(n1, n2, n3)))
+print('E o menor número digititado foi o {}.'.format(min(n1, n2, n3)))
