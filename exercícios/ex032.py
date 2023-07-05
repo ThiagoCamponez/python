95b29b41338d2fdef7f5109c6694061999c7a0bf
ThiagoCamponez <thiagotradercamponez@gmail.com>
2023-07-05 04:36:31 -0300

    Atualizando ex032 e planilha

--- exercícios/ex032.py
+++ exercícios/ex032.py
@@ -1,5 +1,8 @@
-ano = int(input('Digite um ano: '))
+from datetime import date
+ano = int(input('Digite um ano, ou digite 0 para o ano atual: '))
+if ano == 0:
+    ano = date.today().year
 if (ano % 4 == 0 and ano % 100 != 0) or (ano % 400 == 0):
-    print('Bissexto')
+    print('O ano {} é Bissexto'.format(ano))
 else:
-    print('Não é bissexto')
+    print('O ano {} não é bissexto'.format(ano))
