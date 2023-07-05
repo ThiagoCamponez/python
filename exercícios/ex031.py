a50c7195a1c3d68069627f1563d713d599ffcd26
ThiagoCamponez <thiagotradercamponez@gmail.com>
2023-07-05 04:16:49 -0300

    Atualizado o ex031 e planilha

--- exercícios/ex031.py
+++ exercícios/ex031.py
@@ -1,5 +1,6 @@
-distancia = int(input('Quantos km é a distância da sua viagem: '))
+distancia = float(input('Quantos km é a distância da sua viagem: '))
 if distancia < 201:
-    print('Sua passagem custará R${:.2f}'.format(distancia * 0.50))
+    preço = distancia * 0.50
 else:
-    print('Sua passagem terá o valor de R${:.2f}'.format(distancia * 0.45))
+    preço = distancia * 0.45
+print('Sua passagem terá o valor de R${:.2f}'.format(preço))
