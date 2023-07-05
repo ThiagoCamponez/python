67b849948ee4b90177b3cd462a5cae3524c859d7
ThiagoCamponez <thiagotradercamponez@gmail.com>
2023-07-05 03:52:10 -0300

    Atualização do ex029 e planilha

--- exercícios/ex029.py
+++ exercícios/ex029.py
@@ -1,3 +1,8 @@
-velocidade = int(input('Qual a velocidade atual? '))
+print('-' *40)
+print('Qual a velocidade atual do automóvel? ')
+print('-' *40)
+velocidade = int(input('km/h:  '))
 if velocidade > 80:
-    print('Você foi multado em R${}'.format((velocidade - 80) * 7))
+    print('MULTADO! Você excedeu o limite de velocidade de 80 km/h.')
+    print('Você deve pagar uma multa de R${},00'.format((velocidade - 80) * 7))
+print('Tenha uma ótima viagem e digija com segurança!')
