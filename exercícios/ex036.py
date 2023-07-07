7d5096c30d6de4b8dcc0086138579d69719f53f5
ThiagoCamponez <thiagotradercamponez@gmail.com>
2023-07-07 05:05:53 -0300

    Atualizado o ex036

--- exercícios/ex036.py
+++ exercícios/ex036.py
@@ -1,8 +1,14 @@
-casa = float(input('Qual é o valor da casa? R$'))
+from time import sleep
+print('\033[34m-\033[m' * 38)
+print('\033[1;33m SIMULAÇÃO DE FINANCIAMENTO DE IMÓVEL\033[m')
+print('\033[34m-\033[m' * 38)
+casa = float(input('Valor do imóvel? R$'))
 salario = float(input('Qual o valor do seu salário? R$'))
-anos = int(input('Pagará em quantos anos? '))
+anos = int(input('Parcelado em quantos anos? '))
 parcela = casa / (anos * 12)
+print('\033[1;33mVERIFICANDO...\033[m')
+sleep(2)
 if parcela >= salario * 30 / 100:
-    print('Infelizmente você não pode receber o empréstimo')
+    print('\033[4;31mInfelizmente você não pode receber o empréstimo\033[m')
 else:
-    print('Ok, o empréstimo será liberado')
+    print('\033[4;32mOk, o empréstimo será liberado\033[m')
