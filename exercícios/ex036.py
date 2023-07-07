02579ad3a7a687aaaf169b170e314f77025e2673
ThiagoCamponez <thiagotradercamponez@gmail.com>
2023-07-07 04:17:32 -0300

    Finalizado o ex036

--- exercícios/ex036.py
+++ exercícios/ex036.py
@@ -2,5 +2,7 @@ casa = float(input('Qual é o valor da casa? R$'))
 salario = float(input('Qual o valor do seu salário? R$'))
 anos = int(input('Pagará em quantos anos? '))
 parcela = casa / (anos * 12)
-if parcela > salario:
-    print('EMPRÉSTIMO NEGADO! O valor da ')
+if parcela >= salario * 30 / 100:
+    print('Infelizmente você não pode receber o empréstimo')
+else:
+    print('Ok, o empréstimo será liberado')
