9178a9f39cf37e621cbc49477c8c9803b1f83c69
ThiagoCamponez <thiagotradercamponez@gmail.com>
2023-07-13 04:57:13 -0300

    Atualizado o ex040 e planilha

--- exercícios/ex040.py
+++ exercícios/ex040.py
@@ -2,8 +2,8 @@ n1 = float(input('Digite a primeira nota: '))
 n2 = float(input('Digite a segunda nota: '))
 media = (n1 + n2) / 2
 if media < 5:
-    print('REPROVADO! Média {}'.format(media))
+    print('\033[31mALUNO REPROVADO!\033[m Média {}'.format(media))
 elif media > 5 and media < 6.9:
-    print('RECUPERAÇÃO! Média {}'.format(media))
+    print('\033[33mALUNO EM RECUPERAÇÃO!\033[m Média {}'.format(media))
 else:
-    print('APROVADO! Média {}'.format(media))
+    print('\033[32mALUNO APROVADO!\033[m Média {}'.format(media))
