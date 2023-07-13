534e7d586cbddea1c13cc985b183fe48b1cc031b
ThiagoCamponez <thiagotradercamponez@gmail.com>
2023-07-13 04:18:00 -0300

    Atualizado o ex039 e planilha

--- exercícios/ex039.py
+++ exercícios/ex039.py
@@ -1,10 +1,16 @@
 from datetime import date
-anoNascimento = int(input('Digite o ano de nascimento: '))
-idade = date.today().year - anoNascimento
+print('\033[32m* \033[m' * 15)
+print('\033[34mAlistamento Militar\033[m'.center(36))
+print('\033[32m* \033[m' * 15)
+sexo = str(input('Sexo: Masculino ou Feminino: ')).strip().upper()
+if sexo == 'FEMININO':
+     print('\n\033[4;31mAviso:\033[m Você não é obrigada a fazer o alistamento Militar!')
+anoNascimento = int(input('\nDigite o ano de nascimento: '))
+anoAtual = date.today().year
+idade = anoAtual - anoNascimento
 if idade < 18:
-    print('Você tem {} anos de idade! Ainda faltam {} anos para seu alistamento Militar.'.format(idade, 18 - idade))
+    print('\nEm {}, você completa {} anos de idade! Ainda faltam {} anos para seu alistamento Militar.'.format(anoAtual, idade, 18 - idade))
 elif idade == 18:
-        print('Você tem {} anos de idade! Deve se alistar para o Serviço Militar obrigatório!'.format(idade))
+    print('\nEm {}, você completa {} anos de idade! Deve se alistar para o Serviço Militar obrigatório!'.format(anoAtual, idade))
 else:
-    print('Você tem {} anos de idade! Você deveria ter se alistado à {} anos atrás!'.format(idade, idade - 18))
-
+    print('\nEm {}, você completa {} anos de idade! Você deveria ter se alistado à {} anos atrás!'.format(anoAtual, idade, idade - 18))
