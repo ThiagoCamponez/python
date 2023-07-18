d5bcecab703de742e769274fdf808a66d41c2e5e
ThiagoCamponez <thiagotradercamponez@gmail.com>
2023-07-18 02:44:18 -0300

    Atualizado o ex08 e Planilha

--- exercícios/ex048.py
+++ exercícios/ex048.py
@@ -1,5 +1,7 @@
 soma = 0
+cont = 0
 for c in range(1, 501, 2):
     if c % 3 == 0:
         soma += c
-print(soma)
+        cont += 1
+print('A soma de todos os {} valores solicitados é {}'.format(cont, soma))
