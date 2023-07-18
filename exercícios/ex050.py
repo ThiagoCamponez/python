08feedbf49ae42e2faebc9a639d56c661926f44f
ThiagoCamponez <thiagotradercamponez@gmail.com>
2023-07-18 02:58:46 -0300

    Alterado o ex050 e Planilha

--- exercícios/ex050.py
+++ exercícios/ex050.py
@@ -1,6 +1,8 @@
 soma = 0
+cont = 0
 for c in range(1, 7):
     num = int(input('Digite um número: '))
     if num % 2 == 0:
         soma += num
-print('A soma dos números pares informados é: {}'.format(soma)) 
+        cont += 1
+print('Foi informado {} números PARES e a soma dos números pares é: {}'.format(cont, soma)) 
