1900c080833a2d34074607ae678e5a3ba1d2afda
ThiagoCamponez <thiagotradercamponez@gmail.com>
2023-07-19 01:44:48 -0300

    Atualizado o ex051 e Planilha

--- exercícios/ex051.py
+++ exercícios/ex051.py
@@ -1,4 +1,9 @@
-t1 = int(input('Digite o primeiro termo:'))
+print('=' * 30)
+print('10 TERMOS DE UMA P. A.'.center(30))
+print('=' * 30)
+t1 = int(input('Digite o primeiro termo: '))
 razao = int(input('Digite a razão: '))
-for c in range(t1, 100, razao):
-    print(c)
+decimo = t1 + (10 - 1) * razao
+for c in range(t1, decimo + razao, razao):
+    print(c, end = ' → ')
+print('ACABOU!')
