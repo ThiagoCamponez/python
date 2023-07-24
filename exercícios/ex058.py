c09c4402814c4e873a7b5d794951753ddd7dd5d0
ThiagoCamponez <thiagotradercamponez@gmail.com>
2023-07-24 04:14:04 -0300

    Atualizado o ex058.py e planilha

--- exercícios/ex058.py
+++ exercícios/ex058.py
@@ -1,21 +1,19 @@
 from random import randint
-from time import sleep
 tentativas = 0
-computador = randint(0, 11)
+computador = randint(0, 10)
 print('-' * 70)
 print('Sou seu computador! Adivinhe o número que estou pensando entre 0 e 10.')
 print('-' * 70)
-jogador = int(input('\nQual é o seu palpite? '))
-sleep(1)
-while computador != jogador:
-    if computador > jogador:
-        print('\nÉ mais... Tente novamente!')
-        tentativas += 1
-        sleep(1)
-        jogador = int(input('\nQual é o seu palpite? '))
-    elif computador < jogador:
-        print('\nÉ menos... Tente novamente!')
-        tentativas += 1
-        sleep(1)
-        jogador = int(input('\nQual é o seu palpite? '))
-print('\nVocê acertou com {} tentativas. Parabéns!'.format(tentativas))
+acertou = False
+palpites = 0
+while not acertou:
+    jogador = int(input('\nQual é o seu palpite? '))
+    palpites += 1
+    if jogador == computador:
+        acertou = True
+    else:
+        if jogador < computador:
+            print('Mais... Tente novamente!')
+        else:
+            print('Menos... Tente novamente!')
+print('\nVocê acertou com {} tentativas. Parabéns!'.format(palpites))
