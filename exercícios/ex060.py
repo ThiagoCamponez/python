b50b43e7a9906a10022106993a0f7a088a8031f0
ThiagoCamponez <124745669+ThiagoCamponez@users.noreply.github.com>
2023-07-24 11:13:52 -0300

    Atualizado ex060 calculado matematicamente utilizando o for

--- exercícios/ex060.py
+++ exercícios/ex060.py
@@ -3,13 +3,22 @@
 # fatorial = factorial(num)
 # print('O fatorial de {} é {}.'.format(num, fatorial))
 
-num = int(input('\nDigite um número para calcular seu Fatorial: '))
-c = num           #c é o contador
+# num = int(input('\nDigite um número para calcular seu Fatorial: '))
+# c = num           #c é o contador
+# f = 1
+# print('\nCalculando {}! = '.format(num), end='')
+# while c > 0:
+#     print('{}'.format(c), end='')
+#     print(' x ' if c > 1 else ' = ', end='')
+#     f *= c
+#     c -= 1
+# print('{}\n'.format(f))
+
+num = int(input('Digite um número para calcular seu Fatorial: '))
+print('\nCalculando o {}! = '.format(num), end='')
 f = 1
-print('\nCalculando {}! = '.format(num), end='')
-while c > 0:
+for c in range(num, 0, -1):
     print('{}'.format(c), end='')
     print(' x ' if c > 1 else ' = ', end='')
     f *= c
-    c -= 1
 print('{}\n'.format(f))
