6ac9212eb9e2f8def6d5fbf124ecafd6f11cdb39
ThiagoCamponez <thiagotradercamponez@gmail.com>
2023-07-24 02:41:36 -0300

    Atualizado o ex057 e planilha

--- exercícios/ex057.py
+++ exercícios/ex057.py
@@ -1,5 +1,4 @@
-sexo = ''
-while sexo != 'M' in sexo:
-    sexo = str(input('Digite seu sexo [ M / F ]: ')).strip().upper()
-    if sexo != 'M' or 'F':
-        print('Opção Inválida! Tente novamente.')
+sexo = str(input('Digite seu sexo: [M/F] ')).strip().upper()[0]
+while sexo not in 'MmFf':
+    sexo = str(input('Opção Inválida! Por favor, informe seu sexo: ')).strip().upper()[0]
+print('Sexo {} registrado com sucesso!'.format(sexo))
