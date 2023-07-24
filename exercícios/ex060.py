dbac0b23d0d5d57dddaf8042c8f5546447ac7e9f
ThiagoCamponez <124745669+ThiagoCamponez@users.noreply.github.com>
2023-07-24 10:55:39 -0300

    Atualizado ex060 para calcular matematicamente

--- exercícios/ex060.py
+++ exercícios/ex060.py
@@ -1,4 +1,15 @@
-from math import factorial
-num = int(input('Digite um número para calcular seu Fatorial: '))
-fatorial = factorial(num)
-print('O fatorial de {} é {}.'.format(num, fatorial))
+# from math import factorial
+# num = int(input('Digite um número para calcular seu Fatorial: '))
+# fatorial = factorial(num)
+# print('O fatorial de {} é {}.'.format(num, fatorial))
+
+num = int(input('\nDigite um número para calcular seu Fatorial: '))
+c = num           #c é o contador
+f = 1
+print('\nCalculando {}! = '.format(num), end='')
+while c > 0:
+    print('{}'.format(c), end='')
+    print(' x ' if c > 1 else ' = ', end='')
+    f *= c
+    c -= 1
+print('{}\n'.format(f))
