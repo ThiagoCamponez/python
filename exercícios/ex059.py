d85ff59221c0ef56ef1ecded484965eed6e9a934
ThiagoCamponez <124745669+ThiagoCamponez@users.noreply.github.com>
2023-07-24 10:26:42 -0300

    Alterado o ex059 e planilha

--- exercícios/ex059.py
+++ exercícios/ex059.py
@@ -1,3 +1,4 @@
+from time import sleep
 primeiro = int(input('Primeiro valor: '))
 segundo = int(input('Segundo valor: '))
 resultado = 0
@@ -11,6 +12,7 @@ while opção != 5:
     [ 4 ] Novos números
     [ 5 ] Sair do programa''')
     opção = int(input('\n>>>>> Qual é a sua opção? '))
+    sleep(1)
     if opção == 1:
         resultado = primeiro + segundo
         print('\nO resultado da soma de {} + {} = {}'.format(primeiro, segundo, resultado))
@@ -20,12 +22,20 @@ while opção != 5:
     elif opção == 3:
         if primeiro > segundo:
             resultado = primeiro
-        else:
+        elif segundo > primeiro:
             resultado = segundo
+        else:
+            print('\nSão IGUAIS!')            
         print('Entre {} e {} o MAIOR é o {}!'.format(primeiro, segundo, resultado))
     elif opção == 4:
         print('\nInforme os novos números novamente:')
         primeiro = int(input('Primeiro valor: '))
         segundo = int(input('Segundo valor: '))
-    print('=-=' * 12)
+    elif opção == 5:
+        print('Finalizando..')
+    else:
+        print('Opção Inválida! Tente novamente!')
+    print('=-=' * 11)
+    sleep(2)
 print('Fim do programa! Volte sempre!')
+print('=-=' * 11)
