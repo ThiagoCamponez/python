50e51966a2e35f882a70fe8739a0a536b910b8d6
ThiagoCamponez <thiagotradercamponez@gmail.com>
2023-07-25 02:58:28 -0300

    Atualizado o ex065, Lógica Thiago

--- exercícios/ex065.py
+++ exercícios/ex065.py
@@ -1,8 +1,16 @@
-n = c = total = 0
+n = c = total = maior = menor =  0
 continuar = 'S'
 while continuar == 'S':
     c += 1
     n = int(input('Digite um número: '))
     total += n
+    if c == 1:
+        maior = menor = n
+    else:
+        if n > maior:
+            maior = n
+        elif n < menor:
+            menor = n
     continuar = str(input('Quer continuar? [S/N] ')).strip().upper()[0]
-print('Você digitou {} números e a média é {}'.format(c, total / c))
+print('Você digitou {} números e a média é {:.2f}'.format(c, total / c))
+print('O maior valor foi {} e o menor foi {}'.format(maior, menor))
