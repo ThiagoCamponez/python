568beb3cb7ffdad3ba15286452ac92e9063b52cd
ThiagoCamponez <thiagotradercamponez@gmail.com>
2023-07-27 02:33:45 -0300

    Atualizado o ex069 - parte 1

--- exercícios/ex069.py
+++ exercícios/ex069.py
@@ -1,3 +1,14 @@
-print('-' * 30)
-print('CADAS')
-print('-' * 30)
+while True:
+    print('-' * 30)
+    print('CADASTRE UMA PESSOA'.center(30))
+    print('-' * 30)
+    idade = int(input('Idade: '))
+    sexo = str(input('Sexo: [M/F] ')).strip().upper()[0]
+    while sexo not in 'MF':
+        sexo = str(input('Sexo: [M/F] ')).strip().upper()[0]
+    print('-' * 30)
+    continuar = str(input('Quer continuar? [S/N] ')).strip().upper()[0]
+    while continuar not in 'SN':
+        continuar = str(input('Quer continuar? [S/N] ')).strip().upper()[0]
+    if continuar == 'N':
+        break
