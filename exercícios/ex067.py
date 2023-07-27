f0d030371b6d9265a35e16a7597187321aa4f62f
ThiagoCamponez <124745669+ThiagoCamponez@users.noreply.github.com>
2023-07-27 09:25:35 -0300

    Atualizado o ex067 e planilha

--- exercícios/ex067.py
+++ exercícios/ex067.py
@@ -1,14 +1,9 @@
-res = 0
-num = 1
-while num > 0:
+while True:
     num = int(input('\nQuer ver a tabuada de qual valor? '))
+    print('-' * 35)
     if num < 0:
         break
-    print('-' * 35)
-    cont = 1
-    while cont <= 10:
-        res = num * cont
-        print(f'{num} x {cont} = {res}')
-        cont += 1
+    for c in range(1, 11):
+        print(f'{num} x {c} = {num * c}')
     print('-' * 35)
 print('\nPrograma Tabuada ENCERRADO. Volte sempre!\n')
