93e9cd043e3c5543e5d357b145d1c1ef4f463f2f
ThiagoCamponez <124745669+ThiagoCamponez@users.noreply.github.com>
2023-07-27 10:44:34 -0300

    Atualizado o ex070 e planilha

--- exercícios/ex070.py
+++ exercícios/ex070.py
@@ -1,7 +1,7 @@
 print('-' * 40)
 print('LOJA SUPER BARATÃO'.center(40))
 print('-' * 40)
-total = menor = menorvalor = maiorquemil = 0
+total = menor = menorvalor = totmil = 0
 produtomenorvalor = ''
 while True:
     menor += 1
@@ -9,18 +9,16 @@ while True:
     valor = float(input('Preço: R$'))
     total += valor
     if valor > 1000:
-        maiorquemil += 1
-    if menor == 1:
+        totmil += 1
+    if menor == 1 or valor < menorvalor:
         menorvalor = valor
         produtomenorvalor = produto
-    else:
-        if valor < menorvalor:
-            menorvalor = valor
-            produtomenorvalor = produto
-    continuar = str(input('Quer continuar? [S/N] ')).strip().lower()[0]
+    continuar = ' '
+    while continuar not in 'sn':
+        continuar = str(input('Quer continuar? [S/N] ')).strip().lower()[0]
     if continuar == 'n':
         break
-print('------------- FIM DO PROGRAMA -------------')
-print(f'O total da compra foi {total}')
-print(f'Temos {maiorquemil} produtos custando mais de R$1000.00')
-print(f'E o produto mais barato foi: {produtomenorvalor} que custa R${menorvalor}')
+print('{:-^40}'.format(' FIM DO PROGRAMA '))
+print(f'O total da compra foi R${total:.2f}')
+print(f'Temos {totmil} produtos custando mais de R$1000.00')
+print(f'E o produto mais barato foi: {produtomenorvalor} que custa R${menorvalor:.2f}')
