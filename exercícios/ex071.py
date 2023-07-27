3ffab30f44ae3454bdc44b255a7dcaf0f529fe2c
ThiagoCamponez <124745669+ThiagoCamponez@users.noreply.github.com>
2023-07-27 09:10:34 -0300

    Atualizado o ex071

--- exercícios/ex071.py
+++ exercícios/ex071.py
@@ -1,11 +1,19 @@
 print('=' * 40)
 print('BANCO CAMPONÊZ'.center(40))
 print('=' * 40)
+
 cinquenta = vinte = dez = um = 0
-valor = int(input('Qual valor quer sacar: '))
+
+valor = int(input('Qual valor quer sacar: R$'))
+
 cinquenta = valor // 50
-vinte = ((valor % 50) / 20) // 50
-dez = (valor % 50) / 10
-print(f'Total de {cinquenta} notas de R$50')
-print(f'Total de {vinte} notas de R$20')
-print(f'Total de {dez} notas de R$10')
+vinte = (valor % 50) // 20
+dez = vinte % 20
+um = dez // 1
+
+print(f'Total de {cinquenta} cédulas de R$50')
+print(f'Total de {vinte} cédulas de R$20')
+print(f'Total de {dez} cédulas de R$10')
+print(f'Total de {um} cédulas de R$1')
+print('=' * 40)
+print('Volte sempre ao BANCO CAMPONÊZ! Tenha um ótimo dia!')
