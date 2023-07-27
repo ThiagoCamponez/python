f02646987fb9c1fe1341e216ea3c7c20be772849
ThiagoCamponez <124745669+ThiagoCamponez@users.noreply.github.com>
2023-07-27 11:10:03 -0300

    Atualizado o ex071 e planilha

--- exercícios/ex071.py
+++ exercícios/ex071.py
@@ -1,19 +1,25 @@
 print('=' * 40)
-print('BANCO CAMPONÊZ'.center(40))
+print('{:^40}'.format('BANCO CAMPONÊZ'))
 print('=' * 40)
-
-cinquenta = vinte = dez = um = 0
-
 valor = int(input('Qual valor quer sacar: R$'))
-
-cinquenta = valor // 50
-vinte = (valor % 50) // 20
-dez = vinte % 20
-um = dez // 1
-
-print(f'Total de {cinquenta} cédulas de R$50')
-print(f'Total de {vinte} cédulas de R$20')
-print(f'Total de {dez} cédulas de R$10')
-print(f'Total de {um} cédulas de R$1')
+total = valor
+ced = 50
+totced = 0
+while True:
+    if total >= ced:
+        total -= ced
+        totced += 1
+    else:
+        if totced > 0:
+            print(f'Total de {totced} cédulas de R${ced}')
+        if ced == 50:
+            ced = 20
+        elif ced == 20:
+            ced = 10
+        elif ced == 10:
+            ced = 1
+        totced = 0
+        if total == 0:
+            break
 print('=' * 40)
 print('Volte sempre ao BANCO CAMPONÊZ! Tenha um ótimo dia!')
