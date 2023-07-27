afedd40bcf338d20d4a15a3f2ff5ca4f4f8ab6db
ThiagoCamponez <thiagotradercamponez@gmail.com>
2023-07-27 03:57:05 -0300

    Atualizado ex070 com minha lógica

--- exercícios/ex070.py
+++ exercícios/ex070.py
@@ -2,6 +2,7 @@ print('-' * 40)
 print('LOJA SUPER BARATÃO'.center(40))
 print('-' * 40)
 total = menor = menorvalor = maiorquemil = 0
+produtomenorvalor = ''
 while True:
     menor += 1
     produto = str(input('Nome do Produto: ')).strip().upper()
@@ -11,13 +12,15 @@ while True:
         maiorquemil += 1
     if menor == 1:
         menorvalor = valor
+        produtomenorvalor = produto
     else:
         if valor < menorvalor:
             menorvalor = valor
+            produtomenorvalor = produto
     continuar = str(input('Quer continuar? [S/N] ')).strip().lower()[0]
     if continuar == 'n':
         break
 print('------------- FIM DO PROGRAMA -------------')
 print(f'O total da compra foi {total}')
 print(f'Temos {maiorquemil} produtos custando mais de R$1000.00')
-print(f'E o produto mais barato foi: --- que custa R${menorvalor}')
+print(f'E o produto mais barato foi: {produtomenorvalor} que custa R${menorvalor}')
