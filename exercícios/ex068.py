08f0160cc5c41cc93349e21d7fac3f6cce903a2d
ThiagoCamponez <124745669+ThiagoCamponez@users.noreply.github.com>
2023-07-27 10:00:36 -0300

    Atualizado o ex068
    Meu código estava funcionando normalmente, mais achei o código do professor um pouco melhor, então melhorei o meu código com as dicas....

--- exercícios/ex068.py
+++ exercícios/ex068.py
@@ -2,38 +2,33 @@ from random import randint
 print('=-' * 15)
 print('VAMOS JOGAR PAR OU ÍMPAR'.center(30))
 print('=-' * 15)
-qtdVitorias = 0
+v = 0
 while True:
     computador = randint(0, 10)
     jogador = int(input('Digite um valor: '))
-    parImpar = str(input('Par ou Ímpar? [P/I] ')).strip().upper()[0]
-    soma = computador + jogador
-    deu = ''
-    vencedor = ''
-    if soma % 2 == 0:
-        deu = 'PAR'
-        if parImpar == 'P':
-            vencedor = 'Você VENCEU!\nVamos jogar novamente...'
-            qtdVitorias += 1
+    total = computador + jogador
+    tipo = ' '
+    while tipo not in 'PI':
+        tipo = str(input('Par ou Ímpar? [P/I] ')).strip().upper()[0]
+    print('-' * 55)
+    print(f'Você jogou {jogador} e o Computador {computador}. Total de {total} ', end= '')
+    print('DEU PAR' if total % 2 == 0 else 'DEU IMPAR')
+    print('-' * 55)
+    if tipo  == 'P':
+        if total % 2 == 0:
+            print('Você VENCEU!')
+            v += 1
         else:
-            vencedor = 'Você PERDEU..'
+            print('Você PERDEU..')
             break
-    else:
-        deu = 'ÍMPAR'
-        if parImpar == 'I':
-            vencedor = 'Você VENCEU!\nVamos jogar novamente...'
-            qtdVitorias += 1
+    elif tipo == 'I':
+        if total % 2 == 1:
+            print('Você VENCEU!')
+            v += 1
         else:
-            vencedor = 'Você PERDEU..'
+            print('Você PERDEU..')
             break
-    print('-' * 60)
-    print(f'Você jogou {jogador} e o Computador {computador}. Total de {soma}, DEU **{deu}**'.center(60))
-    print('-' * 60)
-    print(vencedor)
+    print('Vamos jogar novamente...')
     print('=-' * 16)
-print('-' * 60)
-print(f'Você jogou {jogador} e o Computador {computador}. Total de {soma}, DEU **{deu}**'.center(60))
-print('-' * 60)
-print(vencedor)
 print('=-' * 16)
-print(f'GAME OVER! Você venceu {qtdVitorias} vezes.' if vencedor == 'Você PERDEU..' else '')
+print(f'GAME OVER! Você venceu {v} vezes.')
